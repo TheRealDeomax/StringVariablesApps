876d8934cf36a94b77cf6b2641473845991820fa
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-29 22:26:07 -0600

    string interpolation and string literials mixed toget

--- StringVariables/Program.cs
+++ StringVariables/Program.cs
@@ -7,9 +7,16 @@ string filePath = string.Empty;
 
 firstName = "Prajuk";
 lastName = "Nusbaum";
-//filePath = "C:\Temp\Demo";
+
+// string literial
+// filePath = "C:\Temp\Demo";
+
 filePath = @"C:\Temp\Demo";
+
+// string interpolation and string literials
+string testString = $@"The file for {firstName} with {lastName} with {filePath}";
 
 // string interpolation
 Console.WriteLine($"{firstName} {lastName}");
 Console.WriteLine(filePath);
+Console.WriteLine(testString);
