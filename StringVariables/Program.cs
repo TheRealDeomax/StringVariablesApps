f69427371fea416e453f783a133c1ef87ee9c051
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-29 22:20:19 -0600

    string literials

--- StringVariables/Program.cs
+++ StringVariables/Program.cs
@@ -3,10 +3,13 @@
 
 string firstName = string.Empty;
 string lastName = string.Empty;
+string filePath = string.Empty;
 
 firstName = "Prajuk";
 lastName = "Nusbaum";
+//filePath = "C:\Temp\Demo";
+filePath = @"C:\Temp\Demo";
 
 // string interpolation
 Console.WriteLine($"{firstName} {lastName}");
-
+Console.WriteLine(filePath);
