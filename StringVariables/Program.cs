d1b8312f6d814698c9b734d49096383e7fc31fd5
txadmin <txadmin@master01>
2022-12-29 22:11:15 -0600

    Add project files.

---
+++ StringVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿// Common Variables
+// string
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+
+firstName = "Prajuk";
+lastName = "Nusbaum";
+
+// string interpolation
+Console.WriteLine($"{firstName} {lastName}");
+
